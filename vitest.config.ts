86b5905685f0a118cfdf4bac7b506819e26554b8
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-16 20:06:16 +0100

    Updated npm i and db

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./__test__/setup.js",
+    setupFiles: "./__test__/setup.ts",
     deps: {
       fallbackCJS: true,
     },
